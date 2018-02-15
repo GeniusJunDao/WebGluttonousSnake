80bdb2e99a8de023f803a930d04fcc913e2c3244
GeniusJunDao <cjd001113@outlook.com>
2018-02-15 16:19:16 +0800

    beauty the js

--- web/game.js
+++ web/game.js
@@ -1,44 +1,48 @@
-var ID//玩家ID
+var ID //玩家ID
 var d = 1
+
 function game() {
-	window.addEventListener('keydown',function(e){
+	window.addEventListener('keydown', function(e) {
 		var x = e.key;
-		if (x == "Up")
-			{d=1}
-		else if (x == "Down")
-			{d=2}
-		else if (x=="Left")
-			{d=3}
-		else if (x=="right"){
-			d=4
+		if(x == "Up") {
+			d = 1
+		} else if(x == "Down") {
+			d = 2
+		} else if(x == "Left") {
+			d = 3
+		} else if(x == "right") {
+			d = 4
 		}
 	})
-	
-	if ("WebSocket" in window) {
+
+	if("WebSocket" in window) {
 		var Socket = new WebSocket("ws://" + window.location.host + "/game/websocket")
-		Socket.onopen = function () {
-			Socket.onmessage = function (evt) {
-				var ID =JSON.parse( evt.data);
-				console.log("你的ID： "+ID.yourID);
-				Socket.onmessage = function (evt) {
+		Socket.onopen = function() {
+			Socket.onmessage = function(evt) {
+				var ID = JSON.parse(evt.data);
+				console.log("你的ID： " + ID.yourID);
+				Socket.onmessage = function(evt) {
 					paintSnake(JSON.parse(evt.data));
-					Socket.send(JSON.stringify({"d": d}));
+					Socket.send(JSON.stringify({
+						"d": d
+					}));
 				}
-				Socket.send(JSON.stringify({ "ready": "OK" }));
+				Socket.send(JSON.stringify({
+					"ready": "OK"
+				}));
 			}
 		}
-	}
-	else {
+	} else {
 		alert("浏览器不支持WebSocket！");
 	}
 }
 
 function paintSnake(snake) {
-	//console.log(snake.player1)
+	console.log(snake.player1)
 	//console.log(snake.player2)
 	var c = document.getElementById("gs-canvas");
 	var cxt = c.getContext("2d");
-	cxt.fillStyle="#DDDDDD"
-	
+	cxt.fillStyle = "#DDDDDD"
+
 	cxt.fill()
 }
